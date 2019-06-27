b1c0282812fd7d7935b46c3e90266a3fc7d10423
Christine <you@example.com>
2019-06-27 11:15:04 +0300

    adds project class

--- awaards/models.py
+++ awaards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    site_name = models.CharField(max_length=300)
+    url = models.URLField(max_length=300)
+    description =  models.CharField(max_length=300)
+    technologies = models.CharField(max_length=300)
+    designer = models.CharField(max_length=300)
+    screenshot = models.ImageField(upload_to='images/', null=True)
+    user = models.ForeignKey(User, null=True)
