ec080a0e16b0506651dabee61e72c363eda7373d
Christine <you@example.com>
2019-06-27 19:46:35 +0300

    adds models to the admin page

--- awaards/admin.py
+++ awaards/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project,Votes,UsabilityRating,ContentRating,DesignRating
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(ContentRating)
+admin.site.register(UsabilityRating)
+admin.site.register(DesignRating)
+admin.site.register(Votes)
+
