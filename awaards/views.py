5b2e13a6c904ed632f88fc3002b2c57e42a11659
Christine <you@example.com>
2019-06-29 18:51:38 +0300

    creates profile and update view function

--- awaards/views.py
+++ awaards/views.py
@@ -2,11 +2,44 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Project,Profile,DesignRating,UsabilityRating,ContentRating
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import ProfileForm,ProjectForm
 
 # Create your views here.
 
 def home(request):
-    posts= Project.objects.all()
-
     return render(request, 'index.html')
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request, id):
+    user = User.objects.get(id=id)
+    current_user = request.user
+    images = Project.objects.filter(user_id = id)
+    
+
+    try:
+        profile = Profile.objects.get(user_id=id)
+    except ObjectDoesNotExist:
+        return redirect(update_profile, current_user.id)   
+    else:
+    
+        return render(request, 'profile.html',{"user":user, "images":images, "profile":profile})
+
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request,id): 
+    current_user = request.user
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user_id =id
+            profile.save()
+        return redirect(home)
+
+    else:
+        form = ProfileForm()
+    return render(request, 'update_profile.html', {"user": user, "form": form})  
+
