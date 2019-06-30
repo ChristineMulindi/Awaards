69deb44b093f01650352a58cff4d495d7114f69b
Christine <you@example.com>
2019-06-30 03:42:40 +0300

    adds search url confg

--- awaards/urls.py
+++ awaards/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.home,name = 'home')
-   
+    url(r'^$',views.home,name = 'home'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
+    url(r'^update/profile/(\d+)$', views.update_profile, name='update-profile'),
+    url(r'^search/',views.search_by_site, name='search_by_site'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
